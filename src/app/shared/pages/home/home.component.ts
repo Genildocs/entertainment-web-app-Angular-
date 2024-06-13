bc7f3bae92d57c488116e3bce15c91d9dabea68f
genildocs <genildocs@gmail.com>
2024-06-13 07:41:25 -0300

    commit

--- src/app/shared/pages/home/home.component.ts
+++ src/app/shared/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InputSearchComponent } from '../../components/input-search/input-search.component';
+import { TrendingComponent } from '../../components/trending/trending.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [InputSearchComponent],
+  imports: [InputSearchComponent, TrendingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
